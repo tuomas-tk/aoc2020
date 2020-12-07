f944cb9436b9d79ae389168718218c8dbea5308a
Tuomas Karjalainen <tuomas.karj@outlook.com>
2020-12-08 00:37:24 +0200

    Day 7 cleanup

--- 07/7.hs
+++ 07/7.hs
@@ -1,4 +1,3 @@
-import qualified Data.Set as Set
 import Data.List (any)
 import Data.Maybe(fromJust)
 
@@ -9,41 +8,50 @@ getInput = do
 type Bag = (String, [InnerBag])
 type InnerBag = (Int, String)
 
-parseContents :: [String] -> [InnerBag]
-parseContents []            = []
-parseContents ("no" : rest) = []
-parseContents contents      = 
-    [((read num) :: Int, name1 ++ name2)] ++ (parseContents rest)
+parseBagContents :: [String] -> [InnerBag]
+parseBagContents []         = []
+parseBagContents ("no" : _) = []
+parseBagContents contents   = 
+    (read count, concat names) : (parseBagContents rest)
   where
-    [num, name1, name2] = take 3 contents
+    (count : names) = take 3 contents
     rest = drop 4 contents
 
 parseLine :: String -> Bag
 parseLine line =
-    ((concat first), parseContents (drop 2 rest))
+    ((concat first), parseBagContents (drop 2 rest))
   where
     (first, rest) = break (=="bags") (words line)
 
-getInnerBags :: [Bag] -> String -> String -> [String]
-getInnerBags allBags topBag name =
-    ( concat
-      $ map (getInnerBags allBags topBag)
-      -- $ filter (/=topBag)
+getInnerBags :: [Bag] -> String -> [String]
+getInnerBags allBags name =
+    name :
+    (
+      concat
+      $ map (getInnerBags allBags)
       $ map snd
       $ fromJust
       $ lookup name allBags
-    ) ++ [name]
+    )
 
-containsBag :: String -> [String] -> Bool
-containsBag name contents = any (==name) contents
+countInnerBags :: [Bag] -> String -> Int
+countInnerBags allBags name =
+    sum
+    $ map (\(count, name) -> count * (countInnerBags allBags name + 1))
+    $ fromJust
+    $ lookup name allBags
 
 main :: IO ()
 main = do
   input <- getInput
   let bags = map parseLine input
+  
   print
     $ length
-    $ filter (\(_, contents) -> containsBag "shinygold" contents)
-    $ map (\(name, _) -> (name, getInnerBags bags name name))
-    $ filter ((/= "shinygold") . fst)
+    $ filter (any (=="shinygold"))
+    $ map (getInnerBags bags)
+    $ filter (/= "shinygold")
+    $ map fst
     $ bags
+
+  print $ countInnerBags bags "shinygold"
